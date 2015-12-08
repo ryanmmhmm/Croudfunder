c370e54477cb308c7e219257729405d2d1bd0f5d
SCOTT WALSH <SCMWALSH@GMAIL.COM>
2015-12-07 21:47:58 -0500

    Created projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,50 @@
+class ProjectsController < ApplicationController
+
+	def index
+		@projects = Project.all.order(start_date: :asc)
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+	def new
+		@project = Project.new	
+	end
+
+	def edit
+		@project = Project.find(params[:id])
+	end
+
+	def create
+		@project = Project.new(project_params)
+
+		if @project.save
+			redirect_to projects_url
+		else
+			render :new
+		end
+	end
+
+	def update
+		@project = Project.find(params[:id])
+
+		if @project.update_attributes(project_params)
+			redirect_to projects_path(@project)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@project = Project.find(params[:id])
+		@project.destroy
+		redirect_to projects_path
+
+	end
+
+	private
+	def project_params
+		params.require(:project).permit(:title, :start_date, :end_date, :funding_target, :description, rewards_attributes: [:amount, :description, :_destroy])
+	end
+end
