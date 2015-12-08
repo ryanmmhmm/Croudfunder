22dcf696b510edf70a3fe30025acd937a5f13894
ryanmmhmm <magowan.ryan@gmail.com>
2015-12-08 17:43:56 -0500

    defining methods for user sessions new create destroy with logic provided by sorcery

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,19 @@
 class UserSessionsController < ApplicationController
   def new
+    @session = User.new
   end
 
   def create
+    if @session = login(params[:email], params[:password])
+      redirect_back_or_to(:users, notice: 'Login successful')
+    else
+      flash.now[:alert] = 'Login failed'
+      render :new
+    end
   end
 
   def destroy
+    logout
+    redirect_to(:users, notice: 'Logged out!')
   end
 end
