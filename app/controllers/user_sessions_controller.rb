a099f04d52b4fdb4150bf5e9d1deb9193f9163fa
Hershel Nashman <hershel.nashman@gmail.com>
2015-12-08 18:18:20 -0500

    header working

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -14,6 +14,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to(:users, notice: 'Logged out!')
+    redirect_to(:projects, notice: 'Logged out!')
   end
 end
