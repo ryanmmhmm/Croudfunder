e363622acadd0fc1484c3294b83552dd79a8eff5
ryanmmhmm <magowan.ryan@gmail.com>
2015-12-09 16:39:02 -0500

    adding validation for owner_id, commented out while debugging

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,7 @@ class Project < ActiveRecord::Base
   has_one :owner
   has_many :rewards
 
+  # validates :owner_id, presence: true
+
   accepts_nested_attributes_for :rewards, reject_if: :all_blank, allow_destroy: true
 end
