a291dc7f2e7705d6e5399b399fc5e2cc89b85874
Hershel Nashman <hershel.nashman@gmail.com>
2015-12-09 20:33:33 -0500

    validations added for new project

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,12 @@ class Project < ActiveRecord::Base
   has_many :rewards
 
   # validates :owner_id, presence: true
+  validates :title, presence: true
+  validates :funding_goal, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
 
   accepts_nested_attributes_for :rewards, reject_if: :all_blank, allow_destroy: true
 end
