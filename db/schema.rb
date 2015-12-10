1403542ec393f094f2dd021b19061ec371321d5c
Rob Holody <robholody@Robs-MacBook-Pro.local>
2015-12-09 21:10:10 -0500

    Started Project-Index Styling, added base theme styling, Nav Bar, and Slideshow Photo box

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20151209163225) do
     t.string   "picture_url"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "owner_id"
     t.string   "category"
+    t.integer  "owner_id"
   end
 
   create_table "rewards", force: :cascade do |t|
