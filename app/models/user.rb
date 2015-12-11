73c9e78b77d0ec0c4522e1a3ec335a9107e54536
Hershel Nashman <hershel.nashman@gmail.com>
2015-12-11 01:44:02 -0500

    user can see total amount funded

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,24 @@ class User < ActiveRecord::Base
 
   has_many :owned_projects, class_name: 'Project', foreign_key: 'owner_id'
   has_many :pledges
+  has_many :rewards, through: :pledges
+  has_many :projects, through: :rewards, through: :pledges
+
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes["password"] }
   validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
 
   validates :email, uniqueness: true
+
+
+  def total_pledge_amount
+  	rewards.sum(:amount)
+  end
+
+  def total_projects_backed
+  	projects.count(:id)
+  end
+
+
 end
